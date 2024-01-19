7b93ee4f7a506203ad4972977351e669a0a27676
s s <sakasatosh@gmail.com>
2024-01-19 23:15:43 +0900

    feat: return ejs view

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import fastify from 'fastify'
+import path from 'path'
+
+import { fastify } from 'fastify'
+import fastifyView from '@fastify/view'
+import ejs from 'ejs'
 
 const server = fastify()
+server.register(fastifyView, {
+  engine: {
+    ejs: ejs,
+  },
+  root: path.join(__dirname, "../src/views")
+})
 
 server.get('/', async (request, reply) => {
-  return 'Hello, Fastify'
+  return reply.view('index.ejs', { text: 'ejs' })
 })
 
 server.listen({ port: 8080 }, (err, address) => {
