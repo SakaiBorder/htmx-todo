056c5a5077f4fdb67d2a41135fb85ecb3dc61a54
s s <sakasatosh@gmail.com>
2024-01-20 15:33:12 +0900

    fix: show initial data

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import path from 'path'
 import { fastify } from 'fastify'
 import fastifyView from '@fastify/view'
 import ejs from 'ejs'
+import { v4 as uuid } from 'uuid'
 
 const server = fastify()
 server.register(fastifyView, {
@@ -12,8 +13,26 @@ server.register(fastifyView, {
   root: path.join(__dirname, "../src/views")
 })
 
+const todos = [
+  {
+    id: uuid(),
+    title: "Wash Dishes",
+    done: true    
+  },
+  {
+      id: uuid(),
+      title: "Go to Library",
+      done: false
+  },
+  {
+      id: uuid(),
+      title: "Study Math",
+      done: false
+  }
+]
+
 server.get('/', async (request, reply) => {
-  return reply.view('index.ejs', { text: 'ejs' })
+  return reply.view('index.ejs', { todos: todos })
 })
 
 server.listen({ port: 8080 }, (err, address) => {
